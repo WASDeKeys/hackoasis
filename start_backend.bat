586612608a85c0ca2627e5d92d6800a93ebc491a
Sohan Ganguly <sohanganguly141@gmail.com>
2025-09-20 18:01:08 +0530

    .

--- start_backend.bat
+++ start_backend.bat
@@ -1,5 +1,8 @@
 @echo off
-echo Starting Simple Backend Server...
+echo. Starting Simple Backend Server...
 echo.
 node simple_backend.js
+
+echo. Starting React Frontend Server...
+start cmd /k "cd frontend && npm start"
 pause
